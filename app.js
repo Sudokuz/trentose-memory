fd241d3f08515dab67ec6da70be18b92228c767d
Sergey <sergio_lebedev@yahoo.it>
2015-10-28 17:35:52 +0100

    losing bug correction

--- app.js
+++ app.js
@@ -3,20 +3,7 @@ var tmpl = "<li id='NUM'  class='done'>" +
                 "<h3>DATA</h3>" +         
             "</li>";
 var cardcount = parseInt(cardcount);
-/*
 
-    
-        <li id="1" class="done">
-          <h3>2</h3>
-        </li>    
-        <li id="2" class="done">
-          <h3>3</h3>
-        </li>               
-        <li id="3" class="hidden">
-          <h3>4</h3>
-        </li>    
-        
-        */
 // Three main classes of cards:
 // done: when the user has discovered the pair of cards
 // hidden: when the card has not been discovered yet
@@ -34,14 +21,16 @@ $(document).ready(function(){
         //switches all cards to hidden
         $(".done").addClass("hidden").removeClass("done");
         
-        $(".cards li").on("click", function (event) {
+        $(".hidden").click( function (event) {
             
             $(this).addClass("done").removeClass("hidden");
             
             if( $(this).text() == cardcount ) {
                 cardcount++;
             } else {
-                alert("You lost!");   
+                alert("You lost!");
+                $(".cards li").addClass("done").removeClass("hidden");
+                cardcount = NaN;
             }
             
             //win condition
